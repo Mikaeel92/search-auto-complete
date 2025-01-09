9a1d0148d36cea34fb377f1892ceb74ecf30e8de
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-09 11:05:16 +0330

    step five, handleChange on input
    functionality for filter data & typing in input

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [loading, setLoading] = useState(false)
   const [errorMsg, setErrorMsg] = useState(null)
   const [searchParams, setSearchParams] = useState('')
+  const [filteredUsers, setFilteredUsers] = useState([])
+  const [showDropDown, setShowDropDown] = useState(false)
 
   const fetchApi = async () => {
     try {
@@ -26,13 +28,28 @@ const App = () => {
   if(loading) {return <div>Data is Loading Please Wait...</div>}
   if(errorMsg !== null) {return <div>Error, {errorMsg}</div>}
 
+  const handleChange = () => {
+    const input = e.target.value.toLowerCase();
+    setSearchParams(input)
+    if(input.length > 1) {
+      const filteredData = 
+      users && users.length
+      ? users.filter((item) => item.toLowerCase().indexOf(input) > -1)
+      : []
+      setFilteredUsers(filteredData)
+      setShowDropDown(true)
+    } else {
+      setShowDropDown(false)
+    }
+  }
+
   return (
     <div className='flex flex-col item-center justify-center h-screen w-screen bg-gray-200'>
       <input
         placeholder='Search Users Here...' 
         className='p-2 bg-purple-500 text-white font-bold rounded-md w-[600px] mx-auto'
         value={searchParams}
-        onChange={setSearchParams}
+        onChange={handleChange}
       />
     </div>
   )
