a94687626618cdaa5fcbc03d95f12077c587a6a6
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-09 10:44:10 +0330

    step two, states & fetch with useEffect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  const fetchApi = async () => {
+    try {
+        setLoading(true)
+        const res = await fetch('https://dummyjson.com/users')
+        const data = await res.json()
+        if(data && data.users && data.users.length) {
+          setUsers(data.users.map((item) => item.firstName))
+            setLoading(false)}}
+    catch (e) {
+        setErrorMsg(e.message)
+        setLoading(false)}}
+
+  useEffect(() => {
+    fetchApi()
+  },[])
+  
   return (
     <div>App</div>
   )
