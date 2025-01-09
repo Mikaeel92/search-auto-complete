3de3db5deff4c3f458f953e6ba0d4a355983c11d
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-09 11:21:07 +0330

    last step & done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,63 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import Suggestion from './Suggestion'
 
-const App = () => {
+const SearchAutoComplete = () => {
 
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [errorMsg, setErrorMsg] = useState(null)
-  const [searchParams, setSearchParams] = useState('')
-  const [filteredUsers, setFilteredUsers] = useState([])
-  const [showDropDown, setShowDropDown] = useState(false)
+    const [users, setUsers] = useState([])
+    const [searchParams, setSearchParams] = useState('')
+    const [ShowDropDown, setShowDropDown] = useState(false)
+    const [filteredUsers, setFilteredUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [errorMsg, setErrorMsg] = useState(null)
 
-  const fetchApi = async () => {
-    try {
-        setLoading(true)
-        const res = await fetch('https://dummyjson.com/users')
-        const data = await res.json()
-        if(data && data.users && data.users.length) {
-          setUsers(data.users.map((item) => item.firstName))
+    const fetchApi = async () => {
+        try {
+            setLoading(true)
+            const res = await fetch('https://dummyjson.com/users')
+            const data = await res.json()
+            if(data && data.users && data.users.length) {
+                setUsers(data.users.map((item) => item.firstName))
+                setLoading(false)}}
+        catch (e) {
+            setErrorMsg(e.message)
             setLoading(false)}}
-    catch (e) {
-        setErrorMsg(e.message)
-        setLoading(false)}}
 
-  useEffect(() => {
-    fetchApi()
-  },[])
-  
-  if(loading) {return <div>Data is Loading Please Wait...</div>}
-  if(errorMsg !== null) {return <div>Error, {errorMsg}</div>}
+    useEffect(() => {
+        fetchApi()
+    }, [])
 
-  const handleChange = () => {
-    const input = e.target.value.toLowerCase();
-    setSearchParams(input)
-    if(input.length > 1) {
-      const filteredData = 
-      users && users.length
-      ? users.filter((item) => item.toLowerCase().indexOf(input) > -1)
-      : []
-      setFilteredUsers(filteredData)
-      setShowDropDown(true)
-    } else {
-      setShowDropDown(false)
-    }
-  }
+    const handleChange = (e) => {
+        const input = e.target.value.toLowerCase();
+        setSearchParams(input);
+        if (input.length > 1) {
+          const filteredData =
+            users && users.length
+              ? users.filter((item) => item.toLowerCase().indexOf(input) > -1)
+              : [];
+          setFilteredUsers(filteredData);
+          setShowDropDown(true);
+        } else {
+          setShowDropDown(false);
+        }}
+    const handleClick = (e) => {
+        setShowDropDown(false)
+        setSearchParams(e.target.innerText)
+        setFilteredUsers([])}
 
-  const handleClick = () => {
-
-  }
+    if(loading) {return <div>Data is Loading Please Wait...</div>}
+    if(errorMsg !== null) {return <div>Error, {errorMsg}</div>}
 
   return (
     <div className='flex flex-col item-center justify-center h-screen w-screen bg-gray-200'>
-      <input
+        <input 
+        value={searchParams} onChange={handleChange} 
         placeholder='Search Users Here...' 
-        className='p-2 bg-purple-500 text-white font-bold rounded-md w-[600px] mx-auto'
-        value={searchParams}
-        onChange={handleChange}
-      />
-      { showDropDown && <Suggestion data={filteredUsers} handleClick={handleClick}/> }
+        className='p-2 bg-purple-500 text-white font-bold rounded-md w-[600px] mx-auto'/>
+        {ShowDropDown && <Suggestion handleClick={handleClick} data={filteredUsers}/>}
     </div>
   )
 }
-export default App
+export default SearchAutoComplete
--- vite-project/src/Suggestion.jsx
+++ vite-project/src/Suggestion.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 
-const Suggestion = ({ data, handleClick }) => {
+const Suggestion = ({data, handleClick}) => {
   return (
     <ul className='flex flex-col item-center justify-center'>
-        {
-            data & data.length
-            ? data.map((item, index) => (
-                <li key={index} className='flex item-center justify-center font-bold mt-4' onClick={handleClick}>
-                    {item}
-                </li>
-            ))
-            : null
-        }
-    </ul>
-  )
-}
-
+        {data && data.length 
+        ? data.map((item, index) => <li className='flex item-center justify-center font-bold mt-4'
+        key={index} 
+        onClick={handleClick}>{item}</li>)
+        : null}
+    </ul>)}
 export default Suggestion
